36a3151bd3fabb63f1c32fe2eb863e493875abb3
Christina <christina.dattomo@gmail.com>
2015-03-09 23:52:23 -0500

    Initial Build Commit

---
+++ Witch'sAdventure/Assets/Scripts/Music.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(AudioSource))]
+
+public class Music : MonoBehaviour {
+
+	void Start () {
+		GetComponent<AudioSource>().Play ();	
+	}
+}
