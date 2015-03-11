09364dda466bcdd441ac58fb0c2fc695ed8f1092
Christina <christina.dattomo@gmail.com>
2015-03-11 12:54:11 -0500

    Added title screen, updated level one, added castle scene.

---
+++ Witch'sAdventure/Assets/Scripts/LevelScripts/LevelSwitch.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSwitch : MonoBehaviour {
+
+	public void OnTitleButton(){
+		Application.LoadLevel ("LevelOne");
+	}
+
+	public void OnTriggerEnter(Collider other){
+		switch(other.tag)
+		{
+		case "Castle Entrance":
+			Application.LoadLevel ("Castle Interior");
+			break;
+
+		case "Castle Exit":
+			Application.LoadLevel ("LevelOne");
+			break;
+
+		default:
+			break;
+		}
+
+	}
+
+}
